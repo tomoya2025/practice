645f2b1cc53b2f5df4a123672d02f9e05b359826
tomoya2025 <t.momo1023@gmail.com>
2025-03-05 20:25:36 +0900

    Add files via upload

---
+++ weather/script.js
@@ -0,0 +1,44 @@
+async function getweather() {
+  const API_key = ''; 
+  const cities = ['Tokyo','Osaka','Fukuoka','Sapporo'];
+  const weatherContainer = document.getElementById('weather');
+
+  weatherContainer.innerHTML = '';
+  
+  for (const city of cities) {
+    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=ja&units=metric&appid=${API_key}`;
+    
+    try {
+      const response = await fetch(URL);
+      if (!response.ok) {
+        throw new Error(`HTTPエラー!ステータス: ${response.status}`);
+      }
+      const data = await response.json();
+      
+      const weather = data.weather[0].description;
+      const icon = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+      const temperature = data.main.temp;
+      const windspeed = data.wind.speed;
+
+      const article = document.createElement('article');
+      article.classList.add('p-2', 'border', 'border-success');
+      
+      article.innerHTML = `
+      <h4 class="mb-3 text-decoration-underline">${city}の天気</h4>
+      <section class="weather-data">
+        <div>天気</div><div>${weather}</div>
+        <div>アイコン</div><div><img src="${icon}" alt="天気アイコン"></div>
+        <div>気温</div><div>${temperature}℃</div>
+        <div>風速</div><div>${windspeed}m/s</div>
+      </section>
+      `;
+      
+      weatherContainer.appendChild(article);
+    
+    } catch(error) {
+      console.error('エラー:', error);
+    }
+  }
+}
+
+window.onload = getweather;
